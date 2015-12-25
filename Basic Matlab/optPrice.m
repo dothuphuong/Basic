b564ca26e440f33c996fcdb642cf159128f1d00e
dothuphuong <dothuphuong90@gmail.com>
2015-12-25 18:17:36 +0100

    [Basic Matlab] v2 optPrice: generalized function

--- Basic Matlab/optPrice.m
+++ Basic Matlab/optPrice.m
@@ -1,30 +1,12 @@
 %Written by: Thu Phuong DO
-%Last modified: 2015-12-19
-%Function to determine European option price by different methods
+%Last modified: 2015-12-31
+%Function to determine option price by different methods
 
-function opt = optPrice(pricingMethod, UndlData)
-%Create structure containing the underlying inputs
-%Rate and Dividend Rate in percentage (%)
-%TypeOption: C/Call or P/Put
-
-S0 = UndlData.S0;
-K = UndlData.Strike;
-R = UndlData.Rate/100;
-T = UndlData.Maturity;
-dt = T/UndlData.Nstep;
-sigma = UndlData.sigma/100;
-
-switch UndlData.Type
-    case {'C', 'Call'}
-        Flag = 1;
-    case {'P', 'Put'}
-        Flag = 0;
-end
-c = UndlData.DividendRate;
-
+function opt = optPrice(pricingMethod, UndlData, modelParams)
 switch pricingMethod
-    case 'Binomial'
-        [~ , OptionValue] = binprice(S0, K, R, T,dt, sigma, Flag, c);
-        opt = OptionValue(1,1);
+    case {'Implicit', 'Explicit'}
+        nS = modelParams(1);
+        nT = modelParams(2);
+        opt = optPriceFD(pricingMethod,UndlData, nS, nT);
 end
 end
