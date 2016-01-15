94b2cf8a17d4443e34e6153ae9eedf4bc5ccb5bd
dothuphuong <dothuphuong90@gmail.com>
2016-01-15 15:19:18 +0100

    [Basic Matlab] Add greeks calculation in BS function

--- Basic Matlab/optPriceBS.m
+++ Basic Matlab/optPriceBS.m
@@ -1,10 +1,11 @@
 %Written by: Thu Phuong DO
 %Last modified: 2016-01-10
 %Black-Scholes Closed-formula to determine the European option price
+
 function opt = optPriceBS(UndlData)
+%STEP 1 - Extract underlying parameters from the structure
 %INPUTS
 %UndlData: Structure containing basic pricing parameters
-
 S0 = UndlData.S0;
 K = UndlData.Strike;
 R = UndlData.Rate/100;
@@ -20,14 +21,15 @@ switch UndlData.Type
         sig = -1;
 end
 
+%STEP 2 - Pricing formula
 %Calculate d1 & d2
 d1 = (log(S0/K) + (R - c + 0.5*sigma^2)*T)/(sigma*sqrt(T));
 d2 = d1 - sigma*sqrt(T);
 
 %Nd1 = exercise probability of option
-Nd1 = normcdf(sig*d1,0,1);
+Nd1 = normcdf(sig*d1);
 %Nd2 = adjusted exercise probability of option
-Nd2 = normcdf(sig*d2,0,1);
+Nd2 = normcdf(sig*d2);
 
 switch TypeEx
     case 'European'
@@ -36,5 +38,17 @@ switch TypeEx
         V = NaN;
 end
 
-opt = struct('Price', V, 'Nd1', Nd1, 'Nd2', Nd2);
+%STEP 3 - Calculate the greeks of option
+delta = exp(-c*T)*Nd1;
+gamma = exp(-c*T)*N_density(d1)/(S0*sigma*sqrt(T));
+%Theta per nb of trading days
+theta = (sig*c*S0*exp(-c*T)*Nd1 - S0*N_density(d1)*sigma/(2*sqrt(T)) ...
+        - sig*R*K*exp(-R*T)*Nd2)/252;
+
+%OUTPUTS
+opt = struct('Price', V,'Delta', delta,'Gamma', gamma, 'Theta', theta);
+end
+
+function r = N_density(x)
+    r = exp(-0.5*x^2)/sqrt(2*pi);
 end
